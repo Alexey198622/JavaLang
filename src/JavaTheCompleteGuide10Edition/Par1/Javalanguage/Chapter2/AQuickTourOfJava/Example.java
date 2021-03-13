ac22898489222ea2bd9120c72b8faa9003aa65f3
Alexey198622 <46447207+Alexey198622@users.noreply.github.com>
2021-03-13 22:27:21 +0300

    JavaLang
    Создал разметку и написал простейшую программу на Java

---
+++ src/JavaTheCompleteGuide10Edition/Par1/Javalanguage/Chapter2/AQuickTourOfJava/Example.java
@@ -0,0 +1,7 @@
+package JavaTheCompleteGuide10Edition.Par1.Javalanguage.Chapter2.AQuickTourOfJava;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java.!!!");
+    }
+}
