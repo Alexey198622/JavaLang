a2f544e43c5b389a128eb0129f3436e11c48dd1a
Alexey198622 <46447207+Alexey198622@users.noreply.github.com>
2021-03-14 20:14:05 +0300

    Оператор цикла for

---
+++ src/JavaTheCompleteGuide10Edition/Par1/Javalanguage/Chapter2/AQuickTourOfJava/ForTest.java
@@ -0,0 +1,12 @@
+package JavaTheCompleteGuide10Edition.Par1.Javalanguage.Chapter2.AQuickTourOfJava;
+
+public class ForTest {
+    public static void main(String[] args) {
+        int x;
+
+        for (x = 0; x < 10; x = x + 1) {
+
+            System.out.println("Значение х: " + x);
+        }
+    }
+}
