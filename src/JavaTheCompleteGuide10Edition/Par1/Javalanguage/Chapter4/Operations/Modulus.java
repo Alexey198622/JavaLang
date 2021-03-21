f9079f51b3921a00925a3d64022ba02c71f689c3
Alexey198622 <46447207+Alexey198622@users.noreply.github.com>
2021-03-21 16:00:30 +0300

    Операция деления по модулю

---
+++ src/JavaTheCompleteGuide10Edition/Par1/Javalanguage/Chapter4/Operations/Modulus.java
@@ -0,0 +1,13 @@
+package JavaTheCompleteGuide10Edition.Par1.Javalanguage.Chapter4.Operations;
+
+// Продемонстрировать применение операции %
+
+public class Modulus {
+    public static void main(String[] args) {
+        int x = 42;
+        double y = 42.25;
+
+        System.out.println("x mod 10 = " + x % 10);
+        System.out.println("x mod 10 = " + y % 10);
+    }
+}
