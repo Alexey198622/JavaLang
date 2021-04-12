e7c74569108e0fa149e0514364011ed73fb22c36
Alexey198622 <46447207+Alexey198622@users.noreply.github.com>
2021-04-12 19:09:28 +0300

    Цикл for

---
+++ src/JavaTheCompleteGuide10Edition/Par1/Javalanguage/Chapter5/ControlOperators/ForTick.java
@@ -0,0 +1,14 @@
+package JavaTheCompleteGuide10Edition.Par1.Javalanguage.Chapter5.ControlOperators;
+
+// Продемонстрировать применение оператора цикла for
+
+public class ForTick {
+    public static void main(String[] args) {
+        int n;
+
+        for ( n = 10; n > 0; n--) {
+            System.out.println("такт " + n);
+        }
+
+    }
+}
